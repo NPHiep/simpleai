d289d88c359a2b42a1d78313eadadf3e14ddf082
Nguyen Phi Hiep <phihiepnguyen.vn@gmail.com>
2016-08-22 11:49:49 +0700

    fix route

--- src/routes.php
+++ src/routes.php
@@ -26,11 +26,6 @@ use LINE\LINEBot\Receive\Operation;
 use Slim\Http\Request;
 use Slim\Http\Response;
 
-$app->get('/', function (Request $req, Response $res, $arg) {
-    return "hello world";
-   
-});
-
 $app->post('/callback', function (Request $req, Response $res, $arg) {
     $body = $req->getBody();
     $signatureHeader = $req->getHeader('X-LINE-ChannelSignature');
@@ -136,3 +131,9 @@ $app->post('/callback', function (Request $req, Response $res, $arg) {
 
     return $res->getBody()->write("OK");
 });
+
+$app->get('/', function (Request $req, Response $res, $arg) {
+    return "hello world";
+   
+});
+
